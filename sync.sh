e13883aeb2f13dfe8fd5652459ac68a7ff83115c
Peter Wilson <pete@ipcortex.co.uk>
2018-11-11 22:04:14 +0000

    Updated to include additonal build targets

--- sync.sh
+++ sync.sh
@@ -12,9 +12,15 @@ z80asm -l -b test.asm
 z80asm -l -b test-slow.asm
 z80asm -l -b sio.asm
 
+/Users/pete/tasm/tasm -b -g0 -a -e -fff basic.asm basic.ihx
+/Users/pete/tasm/tasm -b -g0 -a -e -fff -a4 loader.asm loader.ihx
+/Users/pete/tasm/tasm -b -g0 -a -e -fff siolib.asm siolib.ihx
+/Users/pete/tasm/tasm -b -g0 -a -e -fff sio2.asm sio2.ihx
+/Users/pete/tasm/tasm -b -g0 -a -e -fff sio3.asm sio3.ihx
+
 echo "Generate output"
-appmake +hex -b test.bin --org=0
-appmake +hex -b test-slow.bin --org=0
+appmake +hex -b test.bin --org=656
+appmake +hex -b test-slow.bin --org=656
 appmake +hex -b sio.bin --org=0
 cd ..
 

f177e5b57b46ca30fa273f86dedbd95f6a248e59
Peter Wilson <pete@ipcortex.co.uk>
2018-11-14 23:05:50 +0000

    Add basic support for processing Intel Hex files

--- samples/loader.asm
+++ samples/loader.asm
@@ -1,4 +1,5 @@
 #define WRITE_CHR(c) LD A,c \ RST 08H
+#define WRITE_CRLF   LD A,CR \ RST 08H \ LD A,LF \ RST 08H
 
 SIO_A_C   .EQU     80H
 SIO_A_D   .EQU     81H
@@ -7,47 +8,61 @@ SIO_B_D   .EQU     83H
 
 CR        .EQU     0DH
 LF        .EQU     0AH
+BS        .EQU     08H             ; Backspace
+TAB       .EQU     09H             ; Tab
+DEL       .EQU     7fH             ; Delete
 CS        .EQU     0CH             ; Clear screen
 SPC       .EQU     20H
 
-          .ORG     0290H
+          .ORG     0200H
 
 START:    LD    HL, INTRO
           CALL  PRINT
 
 main:     LD    HL, PROMPT
           CALL  PRINT
+          CALL  GET_LINE
 
-          CALL  GET_CHR
+          ; Echo input line
+          LD    HL, INBUF
+
+          WRITE_CHR(CR)
+          WRITE_CHR(LF)
+          ; CALL  PRINT
+
+          CALL  SKIPSPC
+
+          ; CALL  GET_CHR
           CP    'L'                ; Load
           JR    Z, LOAD
           CP    CR                 ; Load
-          JR    Z, main
+          JR    Z, REPEAT
+          CP    LF                 ; Load
+          JR    Z, REPEAT
           CP    'R'
           JR    Z, RUN
           CP    'D'
           JR    Z, DUMP
+          CP    'R'
+          JR    Z, RUN
           RST   08H                ; Echo character
           JP    err
 
 ; ------------------- load
-LOAD:     LD    HL, WHERE
-          CALL  PRINT
-          CALL  GET_HEX           ; OFFSET to apply to hex records. Z flag set if 'break' pressed.
-          JR    Z, err
-          JP  HEX_IMP
+REPEAT:   WRITE_CHR(CR)
+          WRITE_CHR(LF)
+          JP    main
 
-; ------------------- run
-RUN:      LD    HL, WHERE
-          CALL  PRINT
-          CALL  GET_HEX           ; OFFSET to apply to hex records. Z flag set if 'break' pressed.
-          JR    Z, err
-          JP    RUN_AT
+; ------------------- load
+LOAD:     CALL  GET_HEX           ; OFFSET to apply to hex records. Z flag set if 'break' pressed.
+          JP    HEX_IMP
 
 ; ------------------- dump
-DUMP:     LD    HL, WHERE
-          CALL  PRINT
-          CALL  GET_HEX           ; OFFSET to apply to hex records. Z flag set if 'break' pressed.
+RUN:      CALL  GET_HEX           ; OFFSET to apply to hex records. Z flag set if 'break' pressed.
+          PUSH  HL
+          RET
+; ------------------- run
+DUMP:     CALL  GET_HEX           ; OFFSET to apply to hex records. Z flag set if 'break' pressed.
           JR    NZ, cont_dump
 
           ; No value entered so use the stored value as the dump start address
@@ -57,23 +72,37 @@ cont_dump:
           LD    C,5
 
           ; Dump address (start of line)
-dloop2    LD    A,0dh
-          RST   08h
-          LD    A,0Ah
-          RST   08h
+dloop2    WRITE_CHR(CR)
+          WRITE_CHR(LF)
           CALL  WRITE_16          ; 4 hex digits from HL
-
           WRITE_CHR(SPC)
+          LD    DE, DUMP_CHRS+2
           LD    B,16
 
 dloop:    LD    A,(HL)
+          CP    20h
+          JR    C, outdot
+          CP    7fh
+          JR    NC, outdot
+          LD    (DE),A
+          JR    writeout
+outdot:   LD    A,'.'
+          LD    (DE), A
+          LD    A, (HL)
+writeout: INC   DE
           INC   HL
           CALL  WRITE_8
           WRITE_CHR(SPC)
           DJNZ  dloop
+          PUSH  HL
+          LD    HL, DUMP_CHRS
+          CALL  PRINT
+          POP   HL
           DEC   C
           JR    NZ,dloop2
           LD    (DUMP_ADDR), HL
+          WRITE_CHR(CR)
+          WRITE_CHR(LF)
           JP    main
 
 err:      LD    HL, ERROR
@@ -88,7 +117,8 @@ WRITE_16:  PUSH AF
            CALL WRITE_8
            POP  AF
            RET
-WRITE_8:   PUSH BC
+WRITE_8:   PUSH AF
+          PUSH BC
            PUSH DE
            PUSH HL
            LD   DE, HEX_CHRS
@@ -116,14 +146,62 @@ WRITE_8:   PUSH BC
            POP  HL
            POP  DE
            POP  BC
+           POP  AF
            RET
 
 
 ; ----- Load a hex file (from the console input)
-HEX_IMP:  LD    HL, DONE
+_waiting: .TEXT "Waiting..."
+          .DB    0
+_h1:      .TEXT "Here 1" \ .DB 0
+_h2:      .TEXT "Here 2" \ .DB 0
+_h3:      .TEXT "Here 3" \ .DB 0
+_h4:      .TEXT "Here 4" \ .DB 0
+
+invalid:  LD    HL, _NOSTART
+          CALL  PRINT
+          JP    main
+impeof:   LD    HL,_COMPLETE
+          CALL  PRINT
+          JP    main
+rec_err:  LD    HL,_REC_ERR
+          CALL  PRINT
+          JP    main
+here:     LD    HL,_h1
           CALL  PRINT
           JP    main
 
+; Process all lines starting with a ':'
+HEX_IMP:  LD    HL, _waiting
+          CALL  PRINT
+          WRITE_CRLF
+;          :102000000F0E0D0C0B0A0908070605040302010008
+;          :102010000A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A08
+;          :00000001FF
+nextline: CALL  GET_LINE
+          JR    Z, nextline
+          CALL  BUFCHR
+          CP    ':'
+          JP    NZ,invalid
+          ; Accept this line. Format: [LEN 1][ADDR 2][REC_TYPE 1][DATA 2]+[CHKSM 1]
+          CALL  IN_HEX_2  ; Length -> A
+          JR    C, rec_err
+          LD    B,A       ; Length
+          CALL  IN_HEX_4  ; Address - HL
+          JR    C, rec_err
+          ; CALL  here
+          CALL  IN_HEX_2  ; Command - which should be 00. If not then EOF so end.
+          JR    C, rec_err
+          OR    A
+          JR    NZ, impeof
+next_b:   CALL  IN_HEX_2
+          JR    C, rec_err
+          LD   (HL), A
+          INC   HL
+          DJNZ  next_b
+          ; END of that record (ignoring checksum)
+          WRITE_CRLF
+          JR    nextline
 ; ----- Load a hex file (from the console input)
 RUN_AT:   LD    HL, DONE
           CALL  PRINT
@@ -141,33 +219,16 @@ GET_CHR:  XOR   A
 GET_HEX:  LD    HL, 0
           PUSH  BC
           LD    C,0
-NEXT_HC:  XOR   A
-          CALL  GET_CHR
+          CALL  SKIPSPC
           ; End of input?
-          CP    13
+next_hc:  OR    A
           JR    NZ, cont_hc
-          LD    A,C
+fin:      LD    A,C
           OR    A
           POP   BC
           RET
-cont_hc:
+cont_hc:  CALL  HEX_TO_BIN
           ; Is it between 0 and 9?
-          CP    '0'
-          JR    C, NEXT_HC      ; Less than zero so ignore
-          CP    'F'+1
-          JR    NC, NEXT_HC     ; > 'F' so ignore
-          CP    '9'+1
-          JR    NC, letter_hc
-          RST   08H
-          SUB   '0'
-          JR    add_chr
-letter_hc:
-          CP    'A'
-          JR    C, NEXT_HC
-          RST   08H
-          SUB   'A'
-          ADD   A, 10
-
 add_chr:  ADD   HL, HL
           ADD   HL, HL
           ADD   HL, HL
@@ -175,7 +236,139 @@ add_chr:  ADD   HL, HL
           ADD   A, L
           LD    L, A
           INC   C
-          JR    NEXT_HC
+          CALL  BUFCHR
+          JR    next_hc
+
+; -------- IN_HEX_2 - Return 2 hex digit value in A. Set C flag on error
+IN_HEX_2: PUSH  HL
+          LD    L,0     ; Value being built
+          CALL  BUFCHR  ; A -> character
+          CALL  HEX_TO_BIN
+          JR    C,errhex2
+          LD    L,A     ; First byte
+          CALL  BUFCHR  ; A -> character
+          CALL  HEX_TO_BIN
+          JR    C,errhex2
+          LD    H,A     ; Tmp store
+          LD    A,L     ; Current val
+          ADD   A,A
+          ADD   A,A
+          ADD   A,A
+          ADD   A,A
+          ADD   A,H     ; Which will leave carry clear
+          POP   HL
+          OR    A
+          RET
+
+errhex2:  LD    HL,_HEXERR
+          CALL  PRINT
+          POP   HL
+          RET
+
+IN_HEX_4: CALL  IN_HEX_2
+          RET   C
+          LD    H,A
+          CALL  IN_HEX_2
+          LD    L,A
+          RET
+
+; -------- HEX_TO_BIN Char in A - 0-15. 255 if not valid char
+_hexout:    .TEXT  "\r\nCharacter: [" \ .DB 0
+HEX_TO_BIN: CP    '0'
+            JR    C, inv      ; Less than zero so invalid
+            CP    'F'+1
+            JR    NC, inv     ; > 'F' so ignore
+            CP    '9'+1
+            JR    NC, letter_hc
+            SUB   '0'
+            AND   0fh
+            RET
+letter_hc:  CP    'A'
+            JR    C, inv
+            SUB   'A'-10
+            OR    A
+            RET
+inv:        SCF
+            RET
+
+; --------------------- GET_LINE
+GET_LINE: PUSH     HL
+          PUSH     DE
+          PUSH     BC
+          LD       HL, INBUF
+          LD      (INPTR), HL
+          LD       DE, END_INBUF
+          LD       BC, 0
+
+getc:     CALL     GET_CHR
+          CP       CR
+          JR       Z, eol
+          CP       BS
+          JR       Z, bspc
+
+          ; Store in buffer
+          LD      (HL), A
+
+          ; At end of buffer?
+          LD      C,A
+          LD      A,80
+          CP      B
+          JR      Z, getc       ; buffer full
+          INC     HL
+          INC     B
+          LD      A,C
+          RST     08H
+          JR      getc
+
+eol:      XOR     A
+          LD      (HL), A
+          LD      A, B
+          POP     BC
+          POP     DE
+          POP     HL
+          OR      A     ; Z flag set if no characters entered in line
+          RET
+
+bspc:     XOR      A
+          CP       B
+          JR       Z, getc
+
+          ; Delete character
+          DEC      HL
+          LD      (HL), A
+          DEC      B
+          WRITE_CHR(BS);
+          ; WRITE_CHR(DEL);
+          JR       getc
+
+BUFCHR:   PUSH     HL
+          LD       HL, (INPTR)
+          LD       A, (HL)
+          OR       A
+          JR       Z, eb
+          INC      HL
+          LD       (INPTR), HL
+          POP      HL
+          RET
+
+; -------- SKIPSPC
+; Step over spaces and return first non-space character.
+SKIPSPC:  PUSH     HL
+          LD       HL, (INPTR)
+skip      LD       A, (HL)
+          OR       A
+          JR       Z, eb
+          INC      HL
+          CP       SPC
+          JR       Z,skip
+          CP       TAB
+          JR       Z,skip
+          LD       (INPTR), HL
+eb:       POP      HL
+          RET
+
+; --------- GET_HEX_2 - read 2 byte HEX value from input line to A
+; Z flag set on OK
 
 ; --------------------- PRINT - write a string to the terminal
 PRINT:    LD       A,(HL)          ; Get character
@@ -206,16 +399,23 @@ WHERE:    .TEXT "\rADDR? "
           .DB    0
 ERROR:    .TEXT "\r\nUnknown command\r\n"
           .DB    0
+_NOSTART: .TEXT "\r\nNo record start character ':'\r\n"
+          .DB    0
+_REC_ERR: .TEXT "\r\nBad record\r\n"
+          .DB    0
+_COMPLETE:.TEXT "\r\nDownload complete\r\n"
+          .DB    0
+_HEXERR:  .TEXT "\r\nBad hex character\r\n"
+          .DB    0
 HEX_CHRS: .TEXT  "0123456789ABCDEF"
 
 ; ---------------------- VARIABLES
 DUMP_ADDR: .DW    0
-DUMP_BUFFL .DS  132
-
-TEST:      .TEXT 'The quick brown fox jumped a lot'
-
-;data:
-;  DEFB 0xc3, 0x3C, 0xAA, 0x55
-;  DEFB 0
+DUMP_CHRS: .TEXT  "  "
+           .DS   16
+           .DB    0
+INPTR      .DW  INBUF
+INBUF      .DS   80
+END_INBUF  .DB    0
 
 .END

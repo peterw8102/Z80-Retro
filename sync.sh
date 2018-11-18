6678529bd845dc6aa51d68be1d0e763a368485f5
Peter Wilson <pete@ipcortex.co.uk>
2018-11-18 23:31:27 +0000

    New targets

--- sync.sh
+++ sync.sh
@@ -15,11 +15,12 @@ z80asm -l -b sio.asm
 /Users/pete/tasm/tasm -b -g0 -a -e -fff basic.asm basic.ihx
 /Users/pete/tasm/tasm -b -g0 -a -e -fff -a4 loader.asm loader.ihx
 /Users/pete/tasm/tasm -b -g0 -a -e -fff siolib.asm siolib.ihx
+/Users/pete/tasm/tasm -b -g0 -a -e siolib2.asm siolib2.ihx
 /Users/pete/tasm/tasm -b -g0 -a -e -fff sio2.asm sio2.ihx
 /Users/pete/tasm/tasm -b -g0 -a -e -fff sio3.asm sio3.ihx
 
 echo "Generate output"
-appmake +hex -b test.bin --org=656
+appmake +hex -b test.bin --org=8192
 appmake +hex -b test-slow.bin --org=656
 appmake +hex -b sio.bin --org=0
 cd ..

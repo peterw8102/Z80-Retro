39e2184904f71b5c9b9027b6dd413db1929b2f0c
Peter Wilson <pete@ipcortex.co.uk>
2019-02-13 20:13:24 +0000

    Attempt to set flash/ram page registers

--- monitor/main.asm
+++ monitor/main.asm
@@ -1,56 +1,15 @@
-#define WRITE_CHR(c) LD A,c \ RST 08H
-#define WRITE_CRLF   LD A,CR \ RST 08H \ LD A,LF \ RST 08H
-YES       .EQU      1
-NO        .EQU      0
+#target BIN
+#code   CODE,0,8000h
+
+#include "config.asm"
+#include "defs.asm"
+
+#include "libsio.asm"
 
 ; External dependancies
 ; RST 08h - Write a single character to the terminal, character in A
 ; RST 10h - Read one character from the terminal. Block until there is a character. Returned in A
 ; RST 18h - Check whether there is a character available. Non blocking. Z flag set if there is NOT a character waiting.
-
-; If you want to run the monitor from ROM then variables need to be in RAM. Set this
-; label to indicate where you want variables and change the value of FLASH_MON to YES
-VARS_ADDR    .EQU    2000h
-FLASH_MON    .EQU   NO
-
-; Configurable parameters
-LOAD_ADDR   .EQU    01D0h     ; Start address for the main code
-
-; Correct values to write to the Flash register to make bank 0 RAM and bank 1 ROM
-#if FLASH_MON=YES
-SEL_FLSH  .EQU     08h
-#else
-SEL_FLSH  .EQU     80h
-#endif
-; Where to initialise our stack If the stack is already configured set this to zero. If
-; my system I have 32K memory pages so I run everything from the first 32 allowing me to page in
-; data to the upper 32K
-STACK     .EQU     07ff0h
-
-; My 2x32K pages are controlled by an 8bit write-only I/O port. This is the address of
-; the port. The 'B' command writes a value to this port.
-PAGE_REG    .EQU     0E0h     ; I/O address for 8 bit memory page select register (B command)
-
-; Breakpoints in code are handled by replacing the opcode at the break location with a RST instruction. The
-; default is RST 20h. You can change this if your system is using RST 20 for something else.
-BRK_OPCODE  .EQU     0E7h     ; The instruction to use to cause a breakpoint. This must be a RST xx single byte op. Default RST 20h
-BRK_HANDLER .EQU      20h     ; The address at which to origin the break point handler Must match BRK_OPCODE
-
-; The 'U' command replaces the current running monitor with a modified copy at MON_COPY. This
-; is generally used for developing the monitor and doesn't have a lot of use in a running
-; environment. To make the copy a small copy loop is written to unused memory at LOADER.
-MON_COPY    .EQU    2000h
-LOADER      .EQU    7800h
-
-; Character constants
-CR        .EQU     0DH
-LF        .EQU     0AH
-FF        .EQU     0CH
-BS        .EQU     08H             ; Backspace
-TAB       .EQU     09H             ; Tab
-DEL       .EQU     7fH             ; Delete
-CS        .EQU     0CH             ; Clear screen
-SPC       .EQU     20H
 
           .ORG    BRK_HANDLER
 BP_RST     JP      DO_BP
@@ -61,7 +20,7 @@ NUM_BK    .EQU    64
           .ORG    LOAD_ADDR
 
 START:
-#if STACK
+#if STACK != 0
           LD    HL,STACK
           LD    SP,HL
 #endif
@@ -1371,7 +1330,7 @@ _nobpavail:  .TEXT "No BP available\r\n\000"
 ;
 ; The MSB contains enhanced information. Current bits are
 ;   -  XX XXX AAA
-;   AAA - decribes change of for for this instruction:
+;   AAA - decribes change of control for this instruction:
 ;   000 - Normal instruction, no change of control
 ;   001 - Single byte relative jump (eg JR NZ xx)
 ;   010 - Two byte absolute jump (eg JP C xxxx)

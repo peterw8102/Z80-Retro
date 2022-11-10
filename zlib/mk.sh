151e38d166404f3a363076df9be5f8e7cfb8cb35
Peter Wilson <pete@ipcortex.co.uk>
2022-11-10 19:14:31 +0000

    Add some comments on each command

--- zlib/mk.sh
+++ zlib/mk.sh
@@ -1,11 +1,30 @@
 #/bin/sh
 
-# Assemble the library modules to be relocatable.
+# Utilities
+zmac -j -J --rel7 --oo obj,lst ./libutils.asm
+
+# SIO library
 zmac -j -J --rel7 --oo obj,lst ./libsio.asm
+
+# Command based block protocol to communicate over SIO Port B. I use this with a matching
+# application running on a Raspberry Pi that accepts commands from the Z80 and repplies
+# with requested data.
 zmac -j -J --rel7 --oo obj,lst ./libcmd.asm
-zmac -j -J --rel7 --oo obj,lst ./libconsole.asm
+
+# Console input functions
+zmac -j -J --rel7 --oo obj,lst ./libconin.asm
+
+# Console input and line processing functions
+zmac -j -J --rel7 --oo obj,lst ./libconout.asm
+
+# i2c driver
 zmac -j -J --rel7 --oo obj,lst ./libi2c.asm
-zmac -j -J --rel7 --oo obj,lst ./librtc.asm
+
+# SPI driver
 zmac -j -J --rel7 --oo obj,lst ./libspi.asm
+
+# Real time clock driver
+zmac -j -J --rel7 --oo obj,lst ./librtc.asm
+
+# SDCard library
 zmac -j -J --rel7 --oo obj,lst ./libsdc.asm
-# ld80 -o ./monitor.hex -P 008D -D 1800 -O ihex -s - -m zout/libsio.rel zout/main.rel zout/disassembler.rel zout/libconsole.rel

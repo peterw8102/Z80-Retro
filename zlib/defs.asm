19accfb06a9f7ade16e358f4eef1ed2bab9d2dd0
Peter Wilson <pete@ipcortex.co.uk>
2022-11-10 19:00:31 +0000

    Remove macros and simplify

--- zlib/defs.asm
+++ zlib/defs.asm
@@ -1,33 +1,26 @@
 ; DEFINITIONS
-; Set of common definitions that apply to the zlib libraries and describe the;
-; Simple Z80 memory architecture.
+; Set of common definitions that apply to the zlib libraries and describe the
+; Simple Z80 SBC memory architecture.
 
 ; Hardware release. 16 bit number: MMmmvv
-_hwrel     .EQU      20200               ; 2.2
-
-WRITE_CHR  MACRO     chr
-    LD A,&chr
-    RST 08H
-    ENDM
-WRITE_CRLF MACRO
-    LD A,CR
-    RST 08H
-    LD A,LF
-    RST 08H
-    ENDM
+HWREL      .EQU      20200               ; 2.2
 
 ; Character constants
 NULL       .EQU     00H
-CR:        .EQU     0DH
-LF:        .EQU     0AH
-FF:        .EQU     0CH
-BS:        .EQU     08H             ; Backspace
-TAB:       .EQU     09H             ; Tab
-VT:        .EQU     0BH             ; Ctrl-K
-DEL:       .EQU     7fH             ; Delete
-CS:        .EQU     0CH             ; Clear screen
-SPC:       .EQU     20H
-ESC:       .EQU     1BH
+CR         .EQU     0DH
+LF         .EQU     0AH
+FF         .EQU     0CH
+BS         .EQU     08H             ; Backspace
+TAB        .EQU     09H             ; Tab
+VT         .EQU     0BH             ; Ctrl-K
+DEL        .EQU     7fH             ; Delete
+CS         .EQU     0CH             ; Clear screen
+SPC        .EQU     20H
+ESC        .EQU     1BH
+
+; Address of the MMU page control registers
+PG_PORT0    EQU   $60
+PG_CTRL     EQU   $64    ; switches paging on/off (bit 0). Off after boot. Write to 1 after initialisation.
 
 ; Serial port (SIO/0) ports and related definitions
 SIOA_D          .EQU     $81
@@ -38,9 +31,10 @@ SIOB_C          .EQU     $82
 RTS_HIGH        .EQU    11101000b ; 0E8H
 RTS_LOW         .EQU    11101010b ; 0EAH
 
-; I2C DEFINITIONS
-I2C_RD    EQU 1
-I2C_WR    EQU 0
+; I2C HARDWARE
+I2CPORT   EQU 64h
+I2CPORT_H EQU 65h
+I2CIN     EQU 64h
 
 ; SPI DEFINITIONS
 SPIPORT_L EQU 68h             ; With clock LOW
@@ -52,35 +46,4 @@ SPICS_DS  EQU 01h             ; Disable CS to SDCard
 SPIMASK   EQU 00h             ; Keep I2C clock high and inactive!!
 SPICMD    EQU SPIMASK         ; I2C clock OFF, CS active, CLK low
 
-SPI_WRITE MACRO byte
-    LD     A,&byte
-    CALL   SPI_TXB
-    ENDM
-
-SPI_CWRITE MACRO byte
-    LD     A,&byte|40h
-    CALL   SPI_TXB
-    ENDM
-
-; SPI_SBI
-; Read a single bit into the LSB of L. H contains the value to be written to the OP port (bit 1)
-SPI_SBI   MACRO
-          LD     A,H
-          OUT    (SPIPORT_L),A      ; Set Data with clock LOW
-          OUT    (SPIPORT_H),A      ; Make clock high. SD card *should* have shifted a bit out that we can sample.
-          IN     A,(SPIIN)          ; Read in the next bit.
-          RRA                       ; LSB into C...
-          RL     L                  ; ...and from C to LSB of L
-          ENDM
-
-          ; A contains SPICMD on entry
-; SPI_SBO
-; Send the MSBit of D to the slave, leaving D shifted by one position.
-SPI_SBO   MACRO
-          local _spi_tx
-          XOR    A
-          SLA    D                ; MSB into Carry
-          RLA                     ; Move that bit into the accumulator
-_spi_tx:  OUT    (SPIPORT_L),A    ; Output data and clock LOW
-          OUT    (SPIPORT_H),A    ; Clock high. SD car *should* have shifted out it's first bit that we can sample.
-          ENDM
+import macros.asm

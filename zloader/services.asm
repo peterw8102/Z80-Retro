d733d9b9efa381b008389f074d5357ffb710059b
Peter Wilson <pete@ipcortex.co.uk>
2022-12-11 19:33:10 +0000

    Initialise SD card

--- zloader/services.asm
+++ zloader/services.asm
@@ -1,7 +1,7 @@
 import ../zlib/defs.asm
 import config.asm
 
-          extrn  SD_RBLK, SD_WBLK
+          extrn  SD_INIT, SD_RBLK, SD_WBLK
           extrn  APP_STK, PAGE_MP
           extrn  PGADJ
 
@@ -28,7 +28,7 @@ CSEG
 ; All other registers are command specific.
 DISPATCH: LD     (APP_STK),SP   ; Save the application stack
           LD     SP,SP_STK      ; Replace with supervisor stack
-
+          EI
           ; A selects the function.
           DEC    C
           JR     Z,LD_PGSEL     ; CMD 1 - RAM Page Select (map)
@@ -39,7 +39,7 @@ DISPATCH: LD     (APP_STK),SP   ; Save the application stack
           DEC    C
           JR     Z,CHK_CHR      ; CMD 4 - Check for a character, return if available
           DEC    C
-          JR     Z,LD_STDSK     ; CMD 5 - Set logical disk number
+          JR     Z,LD_STDSK     ; CMD 5 - Map logical disk number
           DEC    C
           JR     Z,LD_STDMA     ; CMD 6 - Set memory address to receive SDCard data
           DEC    C
@@ -88,7 +88,7 @@ LD_PGSEL: LD     A,E
 ; Map an application disk number (0-15) to a physical address (1024). By default each
 ; logical disk maps to a physical disk in the same location. This allows the map to change.
 ; This in turn allows an application to 'mount' logical drives
-; HL - the target physical disk number to map
+; HL - the target slot on the SD card to be mapped (0-511)
 ; D  - the logical disk to map to this physical block. 0-15
 ; Translate into a page and offset and store in local memory
 LD_STDSK: LD     A,D
@@ -107,7 +107,7 @@ LD_STDSK: LD     A,D
           ; Need to multiply DE by 64 (<<6)
           XOR    A
           RR     D
-          RR     E             ; LSB of D now in MSB of E and LSB E in Carry
+          RR     E             ; LSB of D now in MSB of E and LSB E in Carry (divide by2)
           RRA
           RR     D
           RR     E
@@ -122,7 +122,8 @@ LD_STDSK: LD     A,D
 
 ; --------- LD_STDMA (CMD 6)
 ; Record the address (in application space) of the SD Card DMA buffer
-; HL - the address in application space
+; A  - Physical page number into which to write data
+; HL - Offset (14 bits) into the physical page number (WHY?!?!!??!?!)
 ; Translate into a page and offset and store in local memory
 LD_STDMA: CALL   PGADJ
           LD     (DMA_PAGE),A
@@ -133,16 +134,16 @@ LD_STDMA: CALL   PGADJ
 ; SDCard Read Block.
 ;   HLDE is the **SECTOR ADDRESS** (32 bits)
 ; The result will be written to the currently defined DMA address
-LD_SDRD:  CALL   _mapdsk
+LD_SDRD:  CALL   _mapdsk              ; HLDE is now the physical offset into the SDCard
 
-          LD     A,(DMA_PAGE)         ; Get the buffer address into page 1
           LD     BC,(DMA_ADDR)
-
+          LD     A,(DMA_PAGE)         ; Get the buffer page into block 1
           OUT    (PG_PORT0+1),A
 
-          CALL    SD_RBLK
+          CALL   SD_INIT
+          CALL   SD_RBLK             ; Get the SDCard data
 
-          CALL   PGREST               ; Reset the application memory page
+          CALL   PGREST              ; Reset the application memory page
           JR     ENDDIS
 
 ; --------- LD_SDWR (CMD 8)
@@ -151,11 +152,12 @@ LD_SDRD:  CALL   _mapdsk
 ; The result will be written to the currently defined DMA address
 LD_SDWR:  CALL   _mapdsk
 
+          LD     BC,(DMA_ADDR)
           LD     A,(DMA_PAGE)         ; Get the buffer address into page 1
           OUT    (PG_PORT0+1),A
 
-          LD     BC,(DMA_ADDR)
-          CALL    SD_WBLK
+          CALL   SD_INIT
+          CALL   SD_WBLK
 
           CALL   PGREST               ; Reset the application memory page
           JR     ENDDIS
@@ -167,12 +169,12 @@ LD_SDWR:  CALL   _mapdsk
 _mapdsk:  LD     A,$C0
           AND    L
           RR     H          ; LS bit of H
-          RRCA              ; Into MSB of A
+          RRA               ; Into MSB of A
           RR     H          ; And again
-          RRCA              ; A = logical disk * 16. Need *2 to get offset into drive table.
+          RRA               ; A = logical disk * 16. Need *2 to get offset into drive table.
           RRCA
           RRCA
-          RRCA              ; Divide by 8
+          RRCA              ; Divide by 8 to get offset into DRVMAP table
           ; A is now the offset into DRVMAP
           PUSH   DE         ; Stack <= LSWord
           PUSH   HL         ; Stack <= MSWord
@@ -197,7 +199,7 @@ _mapdsk:  LD     A,$C0
 
           DSEG
 
-DMA_PAGE   DEFS    1              ; Page the application wants us to wrote SDcard data to
+DMA_PAGE   DEFS    1              ; Page the application wants us to write SDcard data to
 DMA_ADDR   DEFS    2              ; Offset into the page of the DMA buyffer
 
 .END

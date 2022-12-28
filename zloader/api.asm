131ea6958e5851ed6c15d11c93e4db911f736d27
Peter Wilson <pete@ipcortex.co.uk>
2022-12-28 15:57:25 +0000

    Update API codes

--- zloader/api.asm
+++ zloader/api.asm
@@ -1,13 +1,14 @@
 ; Command codes for the RST 30h system API. These are the values that go into the 'C' register
-A_PGSEL      EQU    1h    ; Map a memory page into a block number
-A_TXCHR      EQU    2h    ; Send character to stdout
-A_RXCHR      EQU    3h    ; Wait for/return next character (stdin)
-A_CHKCH      EQU    4h    ; Check for character (stdin)
-A_DSKMP      EQU    5h    ; Disk drive map
-A_DSKDM      EQU    6h    ; Disk set DMA address
-A_DSKRD      EQU    7h    ; Disk read
-A_DSKRW      EQU    8h    ; Disk raw-read
-A_DSKWR      EQU    9h    ; Disk write
+A_PGSEL      EQU    $1    ; Map a memory page into a block number
+A_TXCHR      EQU    $2    ; Send character to stdout
+A_RXCHR      EQU    $3    ; Wait for/return next character (stdin)
+A_CHKCH      EQU    $4    ; Check for character (stdin)
+A_DSKMP      EQU    $5    ; Disk drive map
+A_DSKDM      EQU    $6    ; Disk set DMA address
+A_DSKRD      EQU    $7    ; Disk read
+A_DSKRW      EQU    $8    ; Disk raw-read
+A_DSKWR      EQU    $9    ; Disk write
+A_DSKWW      EQU    $A    ; Disk raw-write
 
 ; ZLoader private calls
-S_DSKDM      EQU    86h   ; Set DMA address to ZLoader buffer (no params)
+S_DSKDM      EQU    $86   ; Set DMA address to ZLoader buffer (no params)

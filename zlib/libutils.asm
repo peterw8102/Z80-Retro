c318f57a529606dd2f6aeb6e9eda56120d25c449
Peter Wilson <pete@ipcortex.co.uk>
2023-01-06 15:17:02 +0000

    Add STRCMP and ADD8T16
    STRCMP - compare's two strings
    ADD8T16 - adds 8 bit A to 16 bit HL

--- zlib/libutils.asm
+++ zlib/libutils.asm
@@ -6,7 +6,7 @@ import defs.asm
 ; PAUSE:    Wait for a short period (block).
 ;
           ; Input functions
-          public PAUSE
+          public PAUSE,STRCMP,ADD8T16
 
           CSEG
 
@@ -19,3 +19,39 @@ del_2:      DEC   L
             JR    NZ,del_1
             POP   HL
             RET
+
+; ---- STRCMP
+; Compare the string in HL with the test string in DE. This is a limited test as follows:
+;  + Both strings are zero terminated.
+;  + The strings are considered to be the same if HL points to a string at least as long as DE
+;    AND that all characters pointed to by HL match those pointed to by DE
+;
+; HL: String to test
+; DE: Required test string
+; A:      Zero if both strings match, Non-zero if the strings do NOT match
+; Z flag: Matches state of 'A'
+STRCMP:     LD    A,(DE)
+            INC   DE
+            OR    A
+            RET   Z         ; At the end of DE and no failures
+            CP    (HL)      ; Match the test string?
+            RET   NZ        ; String pointed to doesn't match
+            LD    A,(HL)
+            OR    A         ; End of HL string?
+            JR    Z,_sfail
+            INC   HL
+            JR    STRCMP
+
+_sfail:     INC   A
+            RET
+
+
+
+; ------ ADD8T16
+; Add A to HL and correctly deal with carry from L to H. HL and A changed. No
+; other registers used or changed.
+ADD8T16:  ADD   L
+          LD    L,A
+          RET   NC
+          INC   H
+          RET

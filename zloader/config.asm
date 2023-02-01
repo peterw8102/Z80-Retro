875b8e0a5a410126dd46219a022b7ef80b9e3d8b
Peter Wilson <pete@ipcortex.co.uk>
2023-02-01 17:42:05 +0000

    Stack needs to go 1.5K below top of memory
    And we're now reserving two RAM pages for ZIOS/Zloader (it's grown!)

--- zloader/config.asm
+++ zloader/config.asm
@@ -8,7 +8,7 @@ NO       .EQU      0
 ; this to zero. The entire monitor runs in the bottom 16K and so runs in a single
 ; memory page. The stack goes below the 512byte reserved area and the libsio history
 ; buffer.
-SP_STK:      .EQU     4000h - 200h - 1024
+SP_STK:      .EQU     4000h - 1024
 
 ; Breakpoints in code are handled by replacing the opcode at the break location with a RST instruction. The
 ; default is RST 20h. You can change this if your system is using RST 20 for something else.
@@ -29,10 +29,11 @@ endif
 ; numbers while RAM resides from page 20h. This can be swapped with a jumper on the
 ; board, although this is more for historical reasons.
 RAM_PG_0    EQU   20h
+FSH_PG_0    EQU   00h
 
 ; The first MMU page into which to load code using the various load commands. Leave this
 ; value unchanged.
-LD_PAGE     EQU   RAM_PG_0 + 1 + IS_DEVEL
+LD_PAGE     EQU   RAM_PG_0 + 1 + IS_DEVEL + IS_DEVEL
 
 ; Decide whether to install the character set (depends whether the graphic card is installed). If
 ; there's no graphic card then CSET can be set to 0 to disable however there are no problems leaving
@@ -48,3 +49,6 @@ endif
 ; of the first RAM page (20h if RAM is high). In debug this needs to be 1 because the real monitor loads
 ; into the first page.
 MN_PG       EQU   RAM_PG_0 + IS_DEVEL
+
+; The extended page for the loader is stored in the next RAM page after MN_PG.
+MN2_PG      EQU   MN_PG+1

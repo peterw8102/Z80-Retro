f9f7c0a43fc681a331f095a6c385dd1cbf500332
Peter Wilson <pete@ipcortex.co.uk>
2023-02-01 17:36:45 +0000

    Support both SDCard slows available on the main board
    Also some work generalising the interface.

--- zlib/libsdc.asm
+++ zlib/libsdc.asm
@@ -19,42 +19,29 @@ import ../zlib/defs.asm
 ;    https://www.microchip.com/forums/m530149.aspx
 
 
-          extrn  SPI_BEG, SPI_END, SPI_INIT, SPI_ACMD, SPI_TXB
+
+          extrn  SPI_BEG, SPI_END, SPI_INIT, SPI_ACMD, SPI_TXB, SPI_SEL
           extrn  SPI_RXB, SPI_RBF, SPI_CM1, SPI_CMN, SPI_BRD, SPI_BWR
 
+          ; For debug
+          ; extrn  WRITE_8, WRITE_16
+
           ; Operational exports
-          public SD_INIT, SD_WBLK, SD_RBLK, SD_PRES
+          public SD_SEL, SD_INIT, SD_RST, SD_WBLK, SD_RBLK, SD_PRES
 
           ; Debug symbols
           public _RSET, _SCND, _OCR, _AINIT, _SBLK, _CMDBUF
           CSEG
 
-; ------ SD_INIT
-; Initialise the SDCard ready to be used. Initialises the SPI interface
-; and then sets the correct operational mode for the SDCard.
-;
-; This function MUST be called before the read/write block operations. This
-; function can be called multiple times. There are no parameters.
-;
-; The SDCard is set to have a block size of 512 bytes,
-SD_INIT:  IN        A,(SPIIN)
-          AND       SDC0_PR
-          RET       Z
-          CALL     SPI_INIT        ; Initialise the SPI interface
-          CALL     SPI_END
-
-          ; SD Card initialisation sequence
-          CALL      _RSET
-          CALL      _SCND
-          CALL      _OCR
-          CALL      _AINIT
-          CALL      _OCR
-
-          ; Set block size to 512 bytes
-          CALL      _SBLK
-          XOR       A
-          INC       A
-          RET
+; ------ SD_RST
+; Force SDCard re-initialisation. This would be required if the
+; SDCard in one of the carriers in replaced (or card removed then
+; re-inserted). Without calling SD_RST the card won't be reset and
+; placed into the correct operational mode.
+SD_RST:  XOR     A
+         LD      (SD_STAT+1),A   ; Card 0 initialisation status
+         LD      (SD_STAT+4),A   ; Card 1 initialisation status
+         RET
 
 ; ------ SD_PRES
 ; The function takes no parameters and returns a two bit result
@@ -76,6 +63,110 @@ SD_PRES:  IN         A,(SPIIN)
           RRCA
           RRCA
           AND        3
+          RET
+
+
+; ------ SD_TST
+; Test whether the currently selected SDCard is installed.
+; This function returns the SPI device number for the selected
+; SDCard. Card 0 returns 0, card 1 returns 1.
+;
+; Return:
+;     Z flag SET if the card is NOT installed.
+;    HL points to SD_STAT block.
+;     A is the SPI device ID for the selected SDCard
+SD_TST:   OR        A             ; Which drive?
+          IN        A,(SPIIN)
+          LD        HL,SD_STAT+2
+          JR        NZ,_sd1
+          JR        _tst1
+
+_sd1:     LD        HL,SD_STAT+5
+_tst1:    AND       (HL)          ; Device number
+          DEC       HL
+          DEC       HL
+          LD        A,(HL)
+          RET                     ; Do nothing if there's no card
+
+; ------ SD_SEL
+; Select one of the SDCards (0 or 1)
+; A: the device number. Anything other than 0 or 1 will cause no device to
+; be selected.
+SD_SEL:   PUSH       BC
+          PUSH       HL
+          LD         B,A
+          LD         A,$FE
+          AND        B           ; Should be zero
+          JR         NZ,_baddev
+          ; Check whether that drive contains a card
+          LD         A,B
+          CALL       SD_TST
+          JR         Z,_baddev
+          LD         A,B
+          LD         (CUR_DEV),A
+          LD         (CUR_SD),HL
+
+          CALL       SPI_SEL
+_baddev:  POP        HL
+          POP        BC
+          RET
+
+
+; ------ SD_INIT
+; Initialise the currently selected SDCard (set via SD_SEL)
+;
+; Initialises the SPI interface and then sets the correct operational
+; mode for the SDCard.
+;
+; This function MUST be called before the read/write block operations. This
+; function can be called multiple times and should be called before all
+; read/write operations.
+;
+; The SDCard is set to have a block size of 512 bytes.
+;
+SD_INIT:  PUSH      HL
+          PUSH      DE
+          PUSH      BC
+          LD        A,(CUR_DEV)     ; Currently installed device
+          CALL      SD_TST
+          JR        NZ,_havdev      ; If zero then card not installed.
+
+          ; There's no device. HL points to the device control block. Clear the init flag
+          ; so that next time, if the card has been inserted, it will be properly initialised.
+          XOR       A
+          INC       HL
+          LD        (HL),A
+          JR        _fin
+
+_havdev:  CALL      SPI_SEL         ; SD_TST returns the SPI device number. Select it.
+
+          ; Does this card need to be initialised?
+          INC       HL
+          LD        A,(HL)
+          OR        A
+          JR        Z,_doinit       ; Card needs initialisation.
+          CALL      SPI_END
+          JR        _fin
+
+_doinit:  INC       A
+          LD        (HL),A
+          CALL      SPI_INIT        ; Initialise the SPI interface
+          CALL      SPI_END
+
+          ; SD Card initialisation sequence
+          CALL      _RSET
+          CALL      _SCND
+          CALL      _OCR
+          CALL      _AINIT
+          CALL      _OCR
+
+          ; Set block size to 512 bytes
+          CALL      _SBLK
+          XOR       A
+          INC       A
+_fin:     POP       BC
+          POP       DE
+          POP       HL
           RET
 
 ; _RSET: Send a card reset command.
@@ -180,10 +271,10 @@ del_2:   DEC   L
 ; HL  - SD address upper word...
 ; DE  - SD address lower word...
 ; BC  - Points to the buffer containing the data to write.
-SD_WBLK:  IN        A,(SPIIN)
-          AND       SDC0_PR
-          RET       Z
-          CALL       SCL_AD
+;
+; NOTE: The correct drive must have been selected in SD_INIT BEFORE attempting
+; to read or write from a drive.
+SD_WBLK:  CALL       SCL_AD
           PUSH       BC             ; Save buffer address
           ; Send command...
           LD         BC,0FF40h + 24
@@ -246,10 +337,10 @@ _tok1:    CALL       SPI_RBF           ; Don't care about HL
           CMP        B
           JR         NZ,_tok1
 
-_gtok:    POP        DE                 ; Where to write the data
+_gtok:    POP        DE                ; Where to write the data
 
           ; Block read 512byte block
-_nrb:     LD         B,0                ; Number of 16 bit words to read. Read 256x2 bytes
+_nrb:     LD         B,0               ; Number of 16 bit words to read. Read 256x2 bytes
           CALL       SPI_BRD
 
           CALL       SPI_END
@@ -270,6 +361,19 @@ SCL_AD:  LD    H,L     ; Multiple by 256
          RET
 
           DSEG
+
+; Maintain two SDCard status blocks. Bytes in ech block are:
+;    0: the SPI device ID (0 or 1)
+;    1: Whether the device has been initialised
+;    2: The presense bit for this device
+CUR_SD   DW    0         ; Points to current SD_STAT block
+CUR_DEV  DB    0         ; Current SDCard (default to card zero)
+SD_STAT  DB    0         ; SDCard 1 SPI device ID
+         DB    0         ; Set to !0 once initialised
+         DB    SDC0_PR   ; Bit mask for this card's presense
+         DB    1         ; SDCard 2 SPI device ID
+         DB    0         ; Set to !0 once initialised
+         DB    SDC1_PR   ; Bit mask for this card's presense
 
 ; _CMDBUF - small buffer for short command responses that can be returned to the caller.
 _CMDBUF:   DEFS     10      ; For short response data

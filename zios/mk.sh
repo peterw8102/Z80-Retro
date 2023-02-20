540b1e38902690a57bf78b9d94c2149d83e85758
Peter Wilson <pete@ipcortex.co.uk>
2023-02-20 12:53:37 +0000

    Add new files and include zlib as a search path

--- zios/mk.sh
+++ zios/mk.sh
@@ -3,26 +3,28 @@
 # Assemble the library modules to be relocatable.
 if [ -z "$RELEASE" ]; then
   echo "Building DEVELOPMENT version"
-  zmac -j -J --rel7 --oo obj,lst ./services.asm
-  zmac -j -J --rel7 --oo obj,lst ./appl.asm
-  zmac -j -J --rel7 --oo obj,lst ./drive.asm
-  zmac -j -J --rel7 --oo obj,lst ./devmap.asm
-  zmac -j -J --rel7 --oo obj,lst ./mempage.asm
-  zmac -j -J --rel7 --oo obj,lst ./pcb.asm
-  zmac -j -J --rel7 --oo obj,lst ./sdblk.asm
-  zmac -j -J --rel7 --oo obj,lst ./nvram.asm
-  zmac -j -J --rel7 --oo obj,lst ./init.asm
-  zmac -j -J --rel7 --oo obj,lst ./process.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./services.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./appl.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./drive.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./devmap.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./mempage.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./pcb.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./sdblk.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./nvram.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./init.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./process.asm
+  zmac -I ../zlib -j -J --rel7 --oo obj,lst ./run.asm
 else
   echo "Building RELEASE version"
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./services.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./appl.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./drive.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./devmap.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./mempage.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./pcb.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./sdblk.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./nvram.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./init.asm
-  zmac -DRELEASE -j -J --rel7 --oo obj,lst ./process.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./services.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./appl.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./drive.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./devmap.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./mempage.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./pcb.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./sdblk.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./nvram.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./init.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./process.asm
+  zmac -I ../zlib -DRELEASE -j -J --rel7 --oo obj,lst ./run.asm
 fi

29555e19701103e933f029525e12313e0727669c
Peter Wilson <pete@ipcortex.co.uk>
2023-02-20 12:17:35 +0000

    Add new exported ZIOS functions

--- zios/zios.asm
+++ zios/zios.asm
@@ -4,7 +4,12 @@
 
 
   ; From mempage.asm
-  extrn P_ALLOC,P_FREE,P_RES,P_MIN,_pages
+  extrn P_ALLOC,P_FREE,P_RES,P_MIN
+  extrn P_ADJ,P_RESTX,P_REST,P_MAP,P_MAPX
+  extrn _pages
 
   ; From process control
-  extrn PR_INIT
+  extrn PR_INIT,PR_RUN,PR_REST
+
+  ; Running code
+  extrn INSTDRV,MAPAPP

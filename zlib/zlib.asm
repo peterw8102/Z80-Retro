9c940d387aada044a9ed6a2bac30efb533446886
Peter Wilson <pete@ipcortex.co.uk>
2023-02-23 18:52:05 +0000

    Add utility function

--- zlib/zlib.asm
+++ zlib/zlib.asm
@@ -1,7 +1,6 @@
 import defs.asm
 
   ; Symbols exported from ZLIB to clients.
-  extrn  STRCMP,ADD8T16,TOUPPER
   extrn  PRINT,PRINT_LN,NL,GET_LINE,SET_LINE,EDT_LINE
   extrn  SKIPSPC,WASTESPC,BUFCHR,BUFCHUP,UNGET,RESINP
   extrn  WRITE_D,WRITE_8,WRITE_16,INHEX,INHEX_2,INHEX_4
@@ -15,3 +14,6 @@ import defs.asm
 
   ; And the RTC/i2c library
   extrn  RTC_INI, RTC_GET, RTC_SET
+
+  ; Utilities
+  extrn  STRCMP,ADD8T16,TOUPPER,DEC2BIN,HEX2BIN,BIN2HEX
